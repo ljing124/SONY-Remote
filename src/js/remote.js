3172771ac2efa8592bac7f5d625fc82204d87727
ljing124 <lj19971229>
2019-11-22 01:25:08 +0800

    separate js from index

--- src/js/remote.js
+++ src/js/remote.js
@@ -1,78 +1,279 @@
+function camera_connect(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"startRecMode","params":[]})});
+}
+
+function camera_disconn(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"stopRecMode","params":[]})});
+}
+
+function camera_liveon(){
+    resized = false;
+    infoon = true;
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"startLiveview","params":[]})})
+    .then(()=>{
+        fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+        body: JSON.stringify({"method": "setLiveviewFrameInfo", "params": [{"frameInfo": true}], "id": 1, "version": "1.0"})});
+        camera_livestream();
+        setTimeout(function(){ liveview_info();},500);
+    });
+}
+
+function camera_liveoff(){
+    resized = false;
+    infoon = false;
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"stopLiveview","params":[]})})
+    .then(()=>{
+        fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+        body: JSON.stringify({"method": "setLiveviewFrameInfo", "params": [{"frameInfo": false}], "id": 1, "version": "1.0"})})
+    })
+}
+
+function camera_acttake(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"actTakePicture","params":[]})})
+    .then(()=>{ setTimeout(function(){ camera_livestream();},1000); })
+}
+
+function camera_shoot(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"actHalfPressShutter","params":[]})})
+    .then(()=>{ camera_acttake(); })
+}
+
+function camera_recon(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"startMovieRec","params":[]})});
+}
+
+function camera_recoff(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"version":"1.0","id":1,"method":"stopMovieRec","params":[]})});
+}
+
+function camera_cancelaf(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"method": "cancelTouchAFPosition", "params": [], "id": 1, "version": "1.0"})});
+}
+
 /**
- * 光圈设置
+ * canvas大小动态适应
+ * @param {string} imgurl 
+ */
+function camera_resize(imgurl){
+    var tmpimg = new Image();
+    resized = true;
+    tmpimg.src = imgurl;
+    tmpimg.onload = function(){
+      var tmpheight = tmpimg.height;
+      var tmpwidth = tmpimg.width; 
+      var scaleh = document.getElementById("liveview").clientHeight / tmpheight;
+      var scalew = document.getElementById("liveview").clientWidth / tmpwidth;
+      canvas.style.width = tmpwidth * ((scaleh>scalew) ? scalew : scaleh) + "px";
+      canvas.style.height = tmpheight * ((scaleh>scalew) ? scalew : scaleh) + "px";
+      canvas.style.top =  (document.getElementById("liveview").clientHeight - parseFloat(canvas.style.height))/2 + "px";
+      fcanvas.style.width = canvas.style.width;
+      fcanvas.style.height = canvas.style.height;
+      fcanvas.style.left = (document.getElementById("liveview").clientWidth - parseFloat(canvas.style.width))/2 + "px";
+      fcanvas.style.top = canvas.style.top;
+    }
+}
+
+/**
+ * 实时预览数据处理
+ */
+async function camera_livestream(){ 
+    try{ 
+        const res = await fetch(liveurl)
+        let reader = res.body.getReader()
+        let chunk = 0
+        let offset = 0
+        let rest = new Uint8Array
+        while (true) {
+            const result = await reader.read()
+            if (result.done) break
+            chunk += result.value.length
+            if(chunk>16*1024*1024){reader.cancel(); setTimeout(function(){ camera_livestream();},10);}
+            let buffer = new Uint8Array(offset+result.value.length)
+            buffer.set(rest)
+            buffer.set(result.value,offset)
+            while(buffer.length>65536){
+                let imgsize = buffer[12] * 65536 + buffer[13] * 256 + buffer[14]
+                let padsize = buffer[15] + imgsize
+                let imgbuffer = buffer.slice(136,136+padsize)
+                if(buffer[1]==1){                                                     //0x01 JPEG数据
+                    let imgblob = new Blob([imgbuffer],{ type: "image/jpeg" });        
+                    let imgurl = URL.createObjectURL(imgblob);
+                    if(!resized) camera_resize(imgurl);
+                    initTexture(gl, program, n, imgurl);
+                } else {                                                              //0x02 frame数据
+                    fcanvas.width = 1000;
+                    let framecnt = buffer[18] * 256 + buffer[19];
+                    for(let i=0;i<framecnt;i++){
+                        let frameleft = imgbuffer[i*16] * 256 + imgbuffer[i*16+1];
+                        let frametop = imgbuffer[i*16+2] * 256 + imgbuffer[i*16+3];
+                        let frameright = imgbuffer[i*16+4] * 256 + imgbuffer[i*16+5];
+                        let framebottom = imgbuffer[i*16+6] * 256 + imgbuffer[i*16+7];
+                        ctx.strokeStyle = "green"
+                        ctx.lineWidth = 5
+                        ctx.rect(frameleft/10,frametop/10,(frameright-frameleft)/10,(framebottom-frametop)/10);
+                        ctx.stroke();
+                    }
+                }
+                buffer = buffer.slice(padsize+136)
+                offset = buffer.length
+            }
+            rest = buffer.slice(0)
+            offset = rest.length
+        }
+    } catch(err){ 
+        setTimeout(function(){ camera_livestream();},10);   //预览画面中断时尝试重连
+    }
+}
+
+
+/**
+ * 实时光圈、快门、ISO显示
+ */
+function liveview_info(){
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"method": "getFNumber", "params": [], "id": 1, "version": "1.0"})})
+    .then((response)=>{
+        fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+        body: JSON.stringify({"method": "getShutterSpeed", "params": [], "id": 1, "version": "1.0"})})
+        .then((response)=>{
+            fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+            body: JSON.stringify({"method": "getIsoSpeedRate", "params": [], "id": 1, "version": "1.0"})})
+            .then((response)=>{
+                response.json().then((data)=>{ 
+                    if(data.result) document.getElementById("i").innerHTML = data.result[0];
+                })
+            });
+            response.json().then((data)=>{ if(data.result) document.getElementById("s").innerHTML = data.result[0]; })
+        });
+        response.json().then((data)=>{ if(data.result) document.getElementById("f").innerHTML = data.result[0]; })
+    });
+    if(infoon) setTimeout(function(){ liveview_info();},1000);
+}
+
+/**
+ * 获取光圈设置范围
  */
 function camera_getf(){
     if(document.getElementById("setpanel").style.display == "block"){
-      document.getElementById("setpanel").style.display = "none";
-      document.getElementById("fsipanel").innerHTML = "";
-      return;
+        document.getElementById("setpanel").style.display = "none";
+        document.getElementById("fsipanel").innerHTML = "";
+        return;
     }
     fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
     body: JSON.stringify({"method": "getAvailableFNumber", "params": [], "id": 1, "version": "1.0"})})
     .then((response)=>{
-      response.json().then((data)=>{
-        if(data.result[1].length>0){
-          document.getElementById("setpanel").style.display = "block";
-          for(let i=0;i<data.result[1].length;i++){
-            document.getElementById("fsipanel").innerHTML += `<a href="javascript:void(0);" class="btn" style="display:inline-block;" onclick="camera_setf('`+ window.btoa(data.result[1][i]) +"\')\">F"+data.result[1][i]+"</a>";
-          }
-        }
-      })
+        response.json().then((data)=>{
+            if(data.result[1].length>0){
+                document.getElementById("setpanel").style.display = "block";
+                for(let i=0;i<data.result[1].length;i++){
+                    document.getElementById("fsipanel").innerHTML += `<a href="javascript:void(0);" class="btn" style="display:inline-block;" onclick="camera_setf('`+ window.btoa(data.result[1][i]) +"\')\">F"+data.result[1][i]+"</a>";
+                }
+            }
+        })
     });
-  }
-  function camera_gets(){
+}
+
+/**
+ * 获取快门设置范围
+ */
+function camera_gets(){
     if(document.getElementById("setpanel").style.display == "block"){
-      document.getElementById("setpanel").style.display = "none";
-      document.getElementById("fsipanel").innerHTML = "";
-      return;
+        document.getElementById("setpanel").style.display = "none";
+        document.getElementById("fsipanel").innerHTML = "";
+        return;
     }
     fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
     body: JSON.stringify({"method": "getAvailableShutterSpeed", "params": [], "id": 1, "version": "1.0"})})
     .then((response)=>{
-      response.json().then((data)=>{
-        if(data.result[1].length>0){
-          document.getElementById("setpanel").style.display = "block";
-          for(let i=0;i<data.result[1].length;i++){
-            document.getElementById("fsipanel").innerHTML += `<a href="javascript:void(0);" class="btn" style="display:inline-block; margin:0.5vw 0.5vw 0.5vw 0.5vw; padding:1vw 1vw 1vw 1vw;" onclick="camera_sets('`+ window.btoa(data.result[1][i]) +"\')\">"+data.result[1][i]+"</a>";
-          }
-        }
-      })
+        response.json().then((data)=>{
+            if(data.result[1].length>0){
+                document.getElementById("setpanel").style.display = "block";
+                for(let i=0;i<data.result[1].length;i++){
+                    document.getElementById("fsipanel").innerHTML += `<a href="javascript:void(0);" class="btn" style="display:inline-block; margin:0.5vw 0.5vw 0.5vw 0.5vw; padding:1vw 1vw 1vw 1vw;" onclick="camera_sets('`+ window.btoa(data.result[1][i]) +"\')\">"+data.result[1][i]+"</a>";
+                }
+            }
+        })
     });
-  }
-  function camera_geti(){
+}
+
+/**
+ * 获取ISO设置范围
+ */
+function camera_geti(){
     if(document.getElementById("setpanel").style.display == "block"){
-      document.getElementById("setpanel").style.display = "none";
-      document.getElementById("fsipanel").innerHTML = "";
-      return;
+        document.getElementById("setpanel").style.display = "none";
+        document.getElementById("fsipanel").innerHTML = "";
+        return;
     }
     fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
     body: JSON.stringify({"method": "getAvailableIsoSpeedRate", "params": [], "id": 1, "version": "1.0"})})
     .then((response)=>{
-      response.json().then((data)=>{
-        if(data.result[1].length>0){
-          document.getElementById("setpanel").style.display = "block";
-          for(let i=0;i<data.result[1].length;i++){
-            document.getElementById("fsipanel").innerHTML += `<a href="javascript:void(0);" class="btn" style="display:inline-block;" onclick="camera_seti('`+ window.btoa(data.result[1][i]) +"\')\">"+data.result[1][i]+"</a>";
-          }
-        }
-      })
+        response.json().then((data)=>{
+            if(data.result[1].length>0){
+                document.getElementById("setpanel").style.display = "block";
+                for(let i=0;i<data.result[1].length;i++){
+                    document.getElementById("fsipanel").innerHTML += `<a href="javascript:void(0);" class="btn" style="display:inline-block; padding:1vw 1.5vw 1vw 1.5vw;" onclick="camera_seti('`+ window.btoa(data.result[1][i]) +"\')\">"+data.result[1][i]+"</a>";
+                }
+            }
+        })
     });
-  }
+}
+
+/**
+ * 设置光圈
+ */
 function camera_setf(fnumber){
     document.getElementById("setpanel").style.display="none"; 
     document.getElementById("fsipanel").innerHTML = "";
     fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
     body: JSON.stringify({"method": "setFNumber", "params": [window.atob(fnumber)], "id": 1, "version": "1.0"})});
 }
+
+/**
+ * 设置快门
+ */
 function camera_sets(shutter){
     document.getElementById("setpanel").style.display="none"; 
     document.getElementById("fsipanel").innerHTML = "";
     fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
     body: JSON.stringify({"method": "setShutterSpeed", "params": [window.atob(shutter)], "id": 1, "version": "1.0"})});
 }
+
+/**
+ * 设置ISO
+ */
 function camera_seti(isospeed){
     document.getElementById("setpanel").style.display="none"; 
     document.getElementById("fsipanel").innerHTML = "";
     fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
     body: JSON.stringify({"method": "setIsoSpeedRate", "params": [window.atob(isospeed)], "id": 1, "version": "1.0"})});
+}
+
+/**
+ * 设置触摸点对焦
+ * @param {event} e 
+ */
+function camera_touchaf(e){
+    if(!cantouch) return;
+    if(!infoon) return;
+    let x = e.pageX - canvas.getBoundingClientRect().left;
+    let y = e.pageY - canvas.getBoundingClientRect().top;
+    x = Math.floor(x / parseFloat(canvas.style.width) * 1000) / 10 ;
+    y = Math.floor(y / parseFloat(canvas.style.height) * 1000) / 10 ;
+    fetch(apiurl,{method:'POST', credentials: 'include', headers: {'Content-Type': 'text/plain;charset=UTF-8'}, 
+    body: JSON.stringify({"method": "setTouchAFPosition", "params": [x,y], "id": 1, "version": "1.0"})})
+    .then((response)=>{
+        response.json().then((data)=>{
+            console.log(data.result[1].AFResult)
+        })
+    });
 }
--- src/js/wgl.js
+++ src/js/wgl.js
@@ -1,3 +1,224 @@
+/**
+ * 默认顶点着色器
+ */
+const vertexShaderSource = `
+attribute vec4 a_Position;
+attribute vec2 a_TextCoord;
+varying vec2 v_TexCoord;
+void main(){
+    gl_Position = a_Position;
+    v_TexCoord = a_TextCoord;
+}
+`
+
+/**
+ * 默认着色器
+ */
+const fragmentShader_raw = `
+precision mediump float;
+uniform sampler2D u_Sampler;
+varying vec2 v_TexCoord;
+void main(){
+    gl_FragColor = texture2D(u_Sampler, v_TexCoord);
+}
+`
+
+/**
+ * 伪彩色
+ */
+const fragmentShader_psdc = `
+precision mediump float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    vec4 originalColor = texture2D(u_Sampler, v_TexCoord);
+    if(originalColor[0]<0.25) {
+        gl_FragColor = vec4(0.0, originalColor[0]*4.0, 1.0, 1.0);
+    } else if(originalColor[0]>0.25 && originalColor[0]<=0.5) {
+        gl_FragColor = vec4(0.0, 1.0, 2.0-originalColor[0]*4.0, 1.0);
+    } else if(originalColor[0]>0.5 && originalColor[0]<=0.75) {
+        gl_FragColor = vec4(originalColor[0]*4.0-2.0, 1.0, 0.0, 1.0);
+    } else {
+        gl_FragColor = vec4(1.0, 4.0-originalColor[0]*4.0, 0.0, 1.0);
+    }
+}
+`
+
+/**
+ * 斑马70%
+ */
+const fragmentShader_zeba = `
+precision highp float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    vec4 originalColor = texture2D(u_Sampler, v_TexCoord);
+    vec2 screenPos = v_TexCoord * vec2(500.0,500.0);
+    int length = int(screenPos[0]-screenPos[1])-6*int((screenPos[0]-screenPos[1])/6.0);
+    if(originalColor[0]+originalColor[1]+originalColor[2] > 2.1) {
+        if(length<4&&length>0 || length>-6&&length<-2){
+            gl_FragColor = originalColor;
+        } else {
+           gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+    } else {
+        gl_FragColor = originalColor;
+    }
+}
+`
+
+/**
+ * 斑马90%
+ */
+const fragmentShader_zeba2 = `
+precision highp float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    vec4 originalColor = texture2D(u_Sampler, v_TexCoord);
+    vec2 screenPos = v_TexCoord * vec2(500.0,500.0);
+    int length = int(screenPos[0]-screenPos[1])-6*int((screenPos[0]-screenPos[1])/6.0);
+    if(originalColor[0]+originalColor[1]+originalColor[2] > 2.7) {
+        if(length<4&&length>0 || length>-6&&length<-2){
+            gl_FragColor = originalColor;
+        } else {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+    } else {
+        gl_FragColor = originalColor;
+    }
+}
+`
+
+/**
+ * 峰值low
+ */
+const fragmentShader_peak = `
+precision mediump float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    vec4 originalColor = texture2D(u_Sampler, v_TexCoord);
+    if(v_TexCoord.x>0.005 && v_TexCoord.x<0.995 && v_TexCoord.y>0.005 && v_TexCoord.y<0.995) {
+        vec4 laColor = texture2D(u_Sampler,v_TexCoord-vec2(0.005,0.0))
+                     + texture2D(u_Sampler,v_TexCoord-vec2(0.0,0.005))
+                     + texture2D(u_Sampler,v_TexCoord+vec2(0.005,0.0))
+                     + texture2D(u_Sampler,v_TexCoord+vec2(0.0,0.005))
+                     - 4.0 * originalColor;
+        if(laColor[0]+laColor[1]+laColor[2]>2.0) {
+            gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+        } else {
+            gl_FragColor = originalColor;
+        }
+    } else {
+        gl_FragColor = originalColor;
+    }
+}
+`
+
+/**
+ * 峰值high
+ */
+const fragmentShader_peak2 = `
+precision mediump float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    vec4 originalColor = texture2D(u_Sampler, v_TexCoord);
+    if(v_TexCoord.x>0.005 && v_TexCoord.x<0.995 && v_TexCoord.y>0.005 && v_TexCoord.y<0.995) {
+        vec4 laColor = texture2D(u_Sampler,v_TexCoord-vec2(0.005,0.0))
+                     + texture2D(u_Sampler,v_TexCoord-vec2(0.0,0.005))
+                     + texture2D(u_Sampler,v_TexCoord+vec2(0.005,0.0))
+                     + texture2D(u_Sampler,v_TexCoord+vec2(0.0,0.005))
+                     - 4.0 * originalColor;
+        if(laColor[0]+laColor[1]+laColor[2]>1.5) {
+            gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+        } else {
+            gl_FragColor = originalColor;
+        }
+    } else {
+        gl_FragColor = originalColor;
+    }
+}
+`
+
+/**
+ * 波形全屏
+ */
+const fragmentShader_wave = `
+precision highp float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    vec4 originalColor = texture2D(u_Sampler, v_TexCoord);
+    vec4 waveColor = vec4(0.0,0.0,0.0,1.0);
+    for(float i = 0.0; i<1.0; i+=0.01) {
+        vec4 tmpColor = texture2D(u_Sampler,vec2(v_TexCoord.x,i));
+        float tmpGray = (tmpColor[0] + tmpColor[1] + tmpColor[2]) / 3.0;
+        if(tmpGray>v_TexCoord.y-0.01 && tmpGray<v_TexCoord.y+0.01) {
+            waveColor += vec4(0.04,0.04,0.04,1.0);
+        }
+    }
+    gl_FragColor = originalColor*0.3 + waveColor*0.7;
+}
+`
+
+/**
+ * 波形分屏
+ */
+const fragmentShader_wave2 = `
+precision highp float;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
+void main() {
+    if(v_TexCoord.x<0.75 && v_TexCoord.y<0.25) {   //gray
+        vec4 waveColor = vec4(0.0,0.0,0.0,1.0);
+        for(float i = 0.0; i<1.0; i+=0.025) {
+            vec4 tmpColor = texture2D(u_Sampler,vec2(v_TexCoord.x*1.33333333,i));
+            float tmpGray = (tmpColor[0] + tmpColor[1] + tmpColor[2]) / 3.0;
+            if(tmpGray>v_TexCoord.y*4.0-0.01 && tmpGray<v_TexCoord.y*4.0+0.01) {
+                waveColor += vec4(0.04,0.04,0.04,1.0);
+            }
+        }
+        gl_FragColor = waveColor;
+    } else if(v_TexCoord.x>0.75 && v_TexCoord.y>0.66666667) {  //red
+        vec4 waveColor = vec4(0.0,0.0,0.0,1.0);
+        for(float i = 0.0; i<1.0; i+=0.025) {
+            vec4 tmpColor = texture2D(u_Sampler,vec2((v_TexCoord.x-0.75)*4.0,i));
+            if(tmpColor[0]>(v_TexCoord.y-0.66666667)*3.0-0.02 && tmpColor[0]<(v_TexCoord.y-0.66666667)*3.0+0.02) {
+                waveColor += vec4(0.04,0.0,0.0,1.0);
+            }
+        }
+        gl_FragColor = waveColor;
+    } else if(v_TexCoord.x>0.75 && v_TexCoord.y<0.66666667 && v_TexCoord.y>0.33333333) {  //green
+        vec4 waveColor = vec4(0.0,0.0,0.0,1.0);
+        for(float i = 0.0; i<1.0; i+=0.025) {
+            vec4 tmpColor = texture2D(u_Sampler,vec2((v_TexCoord.x-0.75)*4.0,i));
+            if(tmpColor[0]>(v_TexCoord.y-0.33333333)*3.0-0.02 && tmpColor[0]<(v_TexCoord.y-0.33333333)*3.0+0.02) {
+                waveColor += vec4(0.0,0.04,0.0,1.0);
+            }
+        }
+        gl_FragColor = waveColor;
+    } else if(v_TexCoord.x>0.75 && v_TexCoord.y<0.33333333) {  //blue
+        vec4 waveColor = vec4(0.0,0.0,0.0,1.0);
+        for(float i = 0.0; i<1.0; i+=0.025) {
+            vec4 tmpColor = texture2D(u_Sampler,vec2((v_TexCoord.x-0.75)*4.0,i));
+            if(tmpColor[0]>v_TexCoord.y*3.0-0.02 && tmpColor[0]<v_TexCoord.y*3.0+0.02) {
+                waveColor += vec4(0.0,0.0,0.04,1.0);
+            }
+        }
+        gl_FragColor = waveColor;
+    } else {
+        gl_FragColor = texture2D(u_Sampler, vec2(v_TexCoord.x*1.33333333,(v_TexCoord.y-0.25)*1.33333333));
+    }
+}
+`
+
+/**
+ * webgl初始化
+ * @param {*} gl 
+ * @param {*} shaderProgram 
+ */
 function initBuffers(gl, shaderProgram) {
     var vertices = new Float32Array([
         -1.0,  1.0,   0.0, 1.0,
@@ -19,6 +240,13 @@ function initBuffers(gl, shaderProgram) {
     return n;
 }
 
+/**
+ * 纹理初始化
+ * @param {*} gl 
+ * @param {*} shaderProgram 
+ * @param {*} n 
+ * @param {*} imgsrc 
+ */
 function initTexture(gl, shaderProgram, n, imgsrc){
     var texture = gl.createTexture();
     var u_Sampler = gl.getUniformLocation(shaderProgram, 'u_Sampler');
@@ -28,6 +256,14 @@ function initTexture(gl, shaderProgram, n, imgsrc){
     return true;
 }
 
+/**
+ * 载入纹理
+ * @param {*} gl 
+ * @param {*} n 
+ * @param {*} texture 
+ * @param {*} u_Sampler 
+ * @param {*} image 
+ */
 function loadTexture(gl, n, texture, u_Sampler,image){
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
     gl.activeTexture(gl.TEXTURE0);
@@ -41,4 +277,66 @@ function loadTexture(gl, n, texture, u_Sampler,image){
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
     window.URL.revokeObjectURL(image.src);
+}
+
+/**
+ * 显示切换处理函数
+ * @param {int} func 
+ */
+function liveview_handler(func){
+    gl = canvas.getContext("webgl");
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.compileShader(vertexShader);
+    switch(func){
+        case 0: fragmentShaderSource = fragmentShader_raw; break;  //原始
+        case 1: fragmentShaderSource = fragmentShader_psdc; break;  //伪色
+        case 3: fragmentShaderSource = fragmentShader_zeba; break;  //斑马70%
+        case 4: fragmentShaderSource = fragmentShader_zeba2; break;  //斑马90%
+        case 5: fragmentShaderSource = fragmentShader_peak; break;  //峰值low
+        case 6: fragmentShaderSource = fragmentShader_peak2; break;  //峰值high
+        case 7: fragmentShaderSource = fragmentShader_wave; break;  //波形全屏
+        case 8: fragmentShaderSource = fragmentShader_wave2; break;  //波形分屏
+        default: fragmentShaderSource = fragmentShader_raw; break;  //原始
+    }
+    fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+    gl.compileShader(fragmentShader);
+    program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+}
+
+function liveview_psdc(){
+    livefunc = (livefunc==0) ? 1 : 0;
+    liveview_handler(livefunc);
+}
+
+function liveview_zeba(){
+    if(livefunc==0) livefunc = 3;
+    else if(livefunc==3) livefunc = 4;
+    else livefunc = 0;
+    liveview_handler(livefunc);
+}
+
+function liveview_peak(){
+    if(livefunc==0) livefunc = 5;
+    else if(livefunc==5) livefunc = 6;
+    else livefunc = 0;
+    liveview_handler(livefunc);
+}
+
+function liveview_wave(){
+    if(livefunc==0) livefunc = 7;
+    else if(livefunc==7) livefunc = 8;
+    else livefunc = 0;
+    liveview_handler(livefunc);
+}
+
+function liveview_luts(){
+    livefunc = 0;
+    liveview_handler(0);
 }
